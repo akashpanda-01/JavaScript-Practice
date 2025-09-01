45160588b1e8961bfbcf279d2cc04696274660f6
akashpanda-01 <1999akashpanda@gmail.com>
2025-09-02 01:12:43 +0530

    Question Answers

---
+++ index.js
@@ -0,0 +1,35 @@
+// JavaScript Progra To Print "Hello World" ?
+// console.log("Hello World");
+// document.write("Hello World");
+// alert("Hello World");
+
+// Add Two Numbers
+// Way-1
+// console.log(2 + 2);
+
+// Way-2
+// let num1 = 50;
+// let num2 = 50;
+// let Addnum = num1 + num2 ;
+// console.log(Addnum);
+
+// Find Square Root
+// function square(num) {
+//     let sqr = Math.sqrt(num);
+//     // console.log(sqr);
+//     return sqr;
+// }
+// console.log(square(50));
+
+// Function Concatenate Two Strings
+function concatenateStrings(str1, str2) {
+    // let expOutput = "";
+    if(str1 === "Hello" && str2 === "World"){
+        console.log(str1 +" ", str2);
+        return;
+       
+    }
+    // console.log(expOutput);
+    
+}
+concatenateStrings("Hello", "World");

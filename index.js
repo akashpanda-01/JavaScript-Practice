9797c3faa038810aadac076b16a2dd37d98e467d
akashpanda-01 <1999akashpanda@gmail.com>
2025-09-12 11:58:14 +0530

    date-12

--- index.js
+++ index.js
@@ -1645,7 +1645,7 @@ for (let i = 1; i <= 3; i++) {
 // let favMovie = "Avatar";
 // let guess = prompt("Guess The Movie..");
 // while (guess != favMovie) {
-  // if(guess != "quit"){
+// if(guess != "quit"){
 //     alert("You Quit...!");
 //     break;
 //   }
@@ -1657,120 +1657,127 @@ for (let i = 1; i <= 3; i++) {
 
 // LOOPS WITH ARRAYS
 let fruits = ["mango", "appel", "banana", "litchi", "orange"];
-for(let i = 0; i<fruits.length; i++){
+for (let i = 0; i < fruits.length; i++) {
   // console.log(i, fruits[i]);
-};
+}
 
 // Backward
-for(let i = fruits.length-1; i>=0; i--){
+for (let i = fruits.length - 1; i >= 0; i--) {
   // console.log(i, fruits[i]);
-};
+}
 
 // =========================LOOPS WITH ARRAY =======================
 let heroes = [
   ["ironman", "spiderman", "thor"],
-  ["superman", "wonder woman", "flash"]
+  ["superman", "wonder woman", "flash"],
 ];
-for(let i = 0; i < heroes.length; i++){
+for (let i = 0; i < heroes.length; i++) {
   // console.log(heroes[i]);
-  for(let j = 0; j < heroes[i].length; j++){
+  for (let j = 0; j < heroes[i].length; j++) {
     // console.log(heroes[i][j]);
-  };
-};
+  }
+}
 
-let student = [["aman", 95], ["akash", 98], ["shradha", 89]];
-for(let i = 0; i<student.length; i++){
+let student = [
+  ["aman", 95],
+  ["akash", 98],
+  ["shradha", 89],
+];
+for (let i = 0; i < student.length; i++) {
   // console.log(student[i]);
-  for(let j = 0; j<student[i].length; j++){
+  for (let j = 0; j < student[i].length; j++) {
     // console.log(student[i][j]);
-  };
-};
+  }
+}
 
 // for of Loop
-let namesOfFruits = ["mango", "banana", "appel"]
-for(fruit of namesOfFruits){
+let namesOfFruits = ["mango", "banana", "appel"];
+for (fruit of namesOfFruits) {
   // console.log(fruit);
-};
+}
 
-let arrNames = [["a", "b", "c"], ["A", "B", "C"]];
-for(nme of arrNames){
+let arrNames = [
+  ["a", "b", "c"],
+  ["A", "B", "C"],
+];
+for (nme of arrNames) {
   // console.log(nme);
-  for(nm of nme){
+  for (nm of nme) {
     // console.log(nm);
-  };
-};
+  }
+}
 
 // ===================== LOOP QUESTION ANSWER =============================
 // Print Number From 1 to 10 using a loop;
-for(let i = 0; i <= 10; i++){
+for (let i = 0; i <= 10; i++) {
   // console.log(i);
-};
+}
 
 //  Print Even Numbers Between 1 to 20 ?
-for(let i = 0; i<=10; i++){
+for (let i = 0; i <= 10; i++) {
   let val = i * 2;
   // console.log(val);
-};
+}
 
-for(let i = 0; i<=20; i++){
-  if(i % 2 === 0){
+for (let i = 0; i <= 20; i++) {
+  if (i % 2 === 0) {
     // console.log(i);
-  };
-};
+  }
+}
 
-for(let i = 0; i<=20; i = i + 2){
+for (let i = 0; i <= 20; i = i + 2) {
   // console.log(i);
-};
+}
 
 // print odd numbers between 1 to 15
-for(let i = 1; i<=15; i = i + 2){
+for (let i = 1; i <= 15; i = i + 2) {
   // console.log(i);
-};
+}
 
-for(let i = 0; i<=15; i++){
-  if( i % 2 != 0){
+for (let i = 0; i <= 15; i++) {
+  if (i % 2 != 0) {
     // console.log(i);
-  };
-};
+  }
+}
 
-for(let i = 0; i<=15; i++){
+for (let i = 0; i <= 15; i++) {
   let val = i * 2 + 1;
   // console.log(val);
-};
+}
 
 // Print Multiplication Table Of 5
-for(let i = 1; i<=10; i++){
+for (let i = 1; i <= 10; i++) {
   let val = i * 5;
   // console.log("5", i, val);
-};
-for(let i = 1; i<=10; i++){
-  let val = i % 5 === 0;         // it wil give us true or false
+}
+for (let i = 1; i <= 10; i++) {
+  let val = i % 5 === 0; // it wil give us true or false
   // console.log("5", i, val);
-};
+}
 
-for(let i = 0; i<=50; i = i + 5){
+for (let i = 0; i <= 50; i = i + 5) {
   // console.log(i);
-};
+}
 
 // Print Number 10 to 1 reverse order
-for(let i = 10; i>=1; i--){
+for (let i = 10; i >= 1; i--) {
   // console.log(i);
-};
+}
 
 // LEVEL 2
 // Find Sum of numbers between 1 to 100
 let numSum = 0;
-for(let i = 1; i<=100; i++){
+for (let i = 1; i <= 100; i++) {
   numSum += i;
-};
+}
 // console.log("Sum is =",numSum);
 
-let n = 100
-let sum1 = n * (n + 1)/ 2;
+let n = 100;
+let sum1 = (n * (n + 1)) / 2;
 // console.log(sum1);
 
 {
-  let number =  Array.from({length: 100},( _, i) => i + 1);
+  let number = Array.from({ length: 100 }, (_, i) => i + 1);
   // console.log(number);
   let sum = number.reduce((acc, crr) => acc + crr, 0);
   // console.log(sum);
@@ -1779,18 +1786,18 @@ let sum1 = n * (n + 1)/ 2;
 {
   let i = 1;
   let sum = 0;
-  while(i <= 100){
+  while (i <= 100) {
     sum = sum + i;
     i++;
     // console.log(sum);
-  };
+  }
   // console.log(sum);
-};
+}
 
 // Recursive Function
 {
-  function summ(n){
-    if(n === 1) return 1;
+  function summ(n) {
+    if (n === 1) return 1;
     return n + summ(n - 1);
   }
 }
@@ -1800,15 +1807,266 @@ let sum1 = n * (n + 1)/ 2;
 {
   let sum = 0;
   let i = 1;
-  while(i <= 50){
-    sum = sum + i;
+  while (i <= 50) {
+    if (i % 2 === 0) {
+      sum = sum + i;
+      // console.log("Adding", i, "Current sum", sum);
+    }
     i++;
-    console.log(sum);
+  }
+  // console.log(sum);
+}
+
+{
+  let sum = 0;
+  for (let i = 0; i <= 50; i++) {
+    if (i % 2 === 0) {
+      sum = sum + i;
+      // console.log(sum);
+    }
+  }
+}
+{
+  let sum = 0;
+  for (let i = 0; i <= 50; i = i + 2) {
+    sum = sum + i;
+    // console.log(sum);
+  }
+}
+
+// Print Squares of numbers
+{
+  let num = 1;
+  while (num <= 10) {
+    let sqr = num * num;
+    // console.log(sqr);
+    num++;
+  }
+}
+{
+  let sqr;
+  for (let i = 1; i <= 10; i++) {
+    if (true) {
+      sqr = i * i;
+      // console.log(sqr);
+    }
+  }
+}
+
+// Count only numbers between 1-50 that are divisible by 3
+{
+  let count = 0;
+  for (let i = 0; i <= 50; i++) {
+    if (i % 3 === 0) {
+      // console.log(i);
+      count = count + 1;
+    }
+  }
+  // console.log(count);
+}
+
+{
+  let count = 0;
+  let i = 1;
+  while (i <= 50) {
+    if (i % 3 === 0) {
+      // console.log(i);
+      count++;
+    }
+    i++;
+  }
+  // console.log(count);
+}
+
+// Print Numbers Between 1-30 that are divisible by both 2 and 5.
+{
+  let countNum;
+  for (let i = 1; i <= 30; i++) {
+    if (i % 2 === 0 && i % 5 === 0) {
+      // console.log(i);
+      countNum = i;
+    }
+    // console.log(countNum);
+  }
+}
+{
+  let i = 1;
+  let count = 0;
+  while (i <= 30) {
+    if (i % 2 === 0 && i % 5 === 0) {
+      count = i;
+      // console.log(count);
+    }
+    i++;
+  }
+}
+
+// print star triangle
+{
+  let n = 5;
+  for (let i = 1; i <= n; i++) {
+    let row = ""
+    for (let j = 1; j <= i; j++) {
+      row += j;
+      row += "* ";
+    };
+    // console.log(row);
   };
+}
+
+{
+  let n = 1
+  for(let i = 5; i>=n; i--){
+    let row = "";
+    for(let j = 1; j<=i; j++){
+      row += "* ";
+    };
+    // console.log(row);
+  };
+}
+
+{
+  let n = 5;
+  let i = n;
+  while(i >= 1){
+    let row = "";
+    let j = 1
+    
+    while(j <= i){
+      row += "* ";
+      j++;
+    };
+    // console.log(row);
+    i--;
+  }
+}
+{
+  let n = 5;
+  let i = 1;
+  while(i <= n){
+    let row = "";
+    let j = 1;
+    while(j <= i){
+      row += "* ";
+      j++;
+    };
+    // console.log(row);
+    i++;
+  }
+}
+
+{
+  // print Number In triangle.
+  let n = 5;
+  for(let i = 1; i <= n; i++){
+    let row = "";
+    for(let j = 1; j <= i; j++){
+      row += j;
+    };
+    // console.log(row);
+  };
+}
+
+{
+  let n = 5;
+  let i = 1;
+  while(i <= n){
+    let row = "";
+    for(let j = 1; j <= i; j++){
+      row += j;
+    };
+    // console.log(row);
+    i++;
+  };
+}
+
+{
+  let n = 5;
+  for(let i = 1; i<=n; i++){
+    let row = "";
+    for(let j = 1; j<=i; j++){
+      row += "* ";
+    };
+    // console.log(row);
+  };
+}
+
+{
+  // Find Faqctorial of a Given Numer 5.
+  let n = 1;
+  for(let i = 1; i<=5; i++){
+    n = n * i;
+    // console.log("n *",i ,"->", n);
+  };
+}
+
+{
+  let n = 1;
+  let i = 1;
+  while(i <= 5){
+    n *= i;
+    // console.log(n);
+    i++;
+  };
+}
+
+{
+  let n = 123;
+  let reverse = 0;
+  let temp = n;
+  for( let i = 1; temp > 0; i++){
+    reverse = reverse * 10 + (temp % 10);
+    temp = (temp - (temp % 10)) / 10;
+  };
+  // console.log(reverse);
 };
 
 {
-  for(let i = 0; i<=50; i++){
+  let n = 542;
+  let temp = n;
+  let reverseVal = 0;
+  while(temp > 0){
+    reverseVal = reverseVal * 10 + (temp % 10);
+    temp = (temp - (temp % 10)) / 10;
+    // temp++;
+  };
+  // console.log(reverseVal);
+};
 
+{
+  // Count How Many digits are in a given number.
+  let num = 234;
+  let temp = num.toString().length;
+  let count = 0;
+  for(let i = 1; i <= temp; i++){
+    count++;
+  }
+  // console.log(count);
+}
+{
+  let num = 234;
+  let temp = num.toString().length;
+  let count = 0;
+  let i = 0;
+  while (i < temp){
+      count++;
+      i++
+    };
+    console.log(count);
+}
+{
+  let num = 234;
+  let count = 0;
+  while(num > 0){
+    count = count + 1;
+    num = Math.floor(num / 10);
+  }
+  console.log(count);
+}
+
+{
+  // Find Largest st Number In a given Number 7593;
+  let n = 7593;
+  let 
   }
 }

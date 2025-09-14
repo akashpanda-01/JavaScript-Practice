aadd5c0454052479ad79661a1821594bb1b602ee
akashpanda-01 <1999akashpanda@gmail.com>
2025-09-14 12:56:22 +0530

    Right Angle Triangle

--- index.js
+++ index.js
@@ -2539,7 +2539,8 @@ for(let i = 1; i <= 10; i++){
         row += "0";
       };
     };
-    console.log(row);
+    // console.logclear
+    // (row);
   }
 }
 {
@@ -2552,6 +2553,33 @@ for(let i = 1; i <= 10; i++){
       } else {
         row += "0";
       };
+    };
+    // console.log(row);
+  };
+}
+{
+  // Print Right Angle Triangle
+  let n = 5;
+  for(let i = 1; i<=n; i++){
+    let row = "";
+    for(let j = 1; j<=n-i; j++){
+      row += " ";
+    };
+    for(let k = 1; k<=i; k++){
+      row += "*";
+    };
+    console.log(row);
+  }
+}
+{
+  let n = 5;
+  for(let i = 1; i <= n; i++){
+    let row = "";
+    for(let j = 1; j <= n-i; j++){
+      row += " ";
+    };
+    for(let k = 1; k<=i; k++){
+      row += "*";
     };
     console.log(row);
   };

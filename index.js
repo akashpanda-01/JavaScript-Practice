f49e23a4c4e17cbb397c588ce2399ca331ebed93
akashpanda-01 <1999akashpanda@gmail.com>
2025-09-16 22:53:38 +0530

    array q/n

--- index.js
+++ index.js
@@ -3273,3 +3273,14 @@ printVowel("hello");
   };
   console.log(count);
 }
+{
+  //Find The missing number in a given array;
+  let arr = [1, 2, 3, 5, 6];
+  let n = 6;
+  let total = (n * (n + 1)) / 2;
+  let sum = 0;
+  for(let i = 0; i<arr.length; i++){
+    sum += arr[i];
+  };
+  console.log(total - sum);
+}

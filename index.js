f860c13164287bf1320ddeba08bf928175f8123d
akashpanda-01 <1999akashpanda@gmail.com>
2025-09-26 13:27:55 +0530

    Q/N1

--- index.js
+++ index.js
@@ -4403,3 +4403,20 @@ printVowel("hello");
  } 
  console.log(validPraentheses("{[()]}"));
 }
+{
+  function isParenthesesValid(str){
+    let stack = []
+    let map = {"}": "{", ")": "(", "]": "["};
+    for(let char of str){
+      if(["(", "{", "["].includes(char)){
+        stack.push(char);
+      } else if([")", "}", "]"].includes(char)){
+         if(stack.pop() !== map[char]){
+          return false;
+         };
+      };
+    };
+    return stack.length === 0;
+  };
+  console.log(isParenthesesValid("({[]})")); 
+}

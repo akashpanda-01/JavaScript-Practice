2ee63a1b4ca441e63d978ff1442a8001a061e647
akashpanda-01 <1999akashpanda@gmail.com>
2025-09-27 19:12:56 +0530

    Date-@7

--- index.js
+++ index.js
@@ -4621,5 +4621,17 @@ printVowel("hello");
   }
 }
 {
-  
+  // 
+  function binarySearch(arr, target, left = 0, right = arr.length - 1) {
+  if (left > right) return -1;
+
+  let mid = Math.floor((left + right) / 2);
+  if (arr[mid] === target) return mid;
+  if (target < arr[mid]) return binarySearch(arr, target, left, mid - 1);
+  return binarySearch(arr, target, mid + 1, right);
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5, 6], 4)); 
+// Output: 3 (index of 4)
+
 }
